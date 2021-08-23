e6f7883a7ebe88bba33e37836e108f4acdd9961f
Alan Arguelho <alanarguelho3@outlook.com>
2021-08-23 09:04:32 -0400

    cors aspas

--- src/serve.js
+++ src/serve.js
@@ -63,7 +63,7 @@ const app = express();
 
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
 
